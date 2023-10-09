5cc0e0a741b1d5530400ec9f6c4160b523187f12
Ghani Azh <ghani.alaziz17@gmail.com>
2023-10-09 16:07:55 +0700

    update

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <>
 
     {/* Section 1 */}
-    <div className="h-screen text-center">
+    <div data-aos="fade-down" className="h-screen text-center">
         <div className="h-[70%] w-full bg-ijo flex flex-col items-center justify-center pt-4">
           <h1 data-aos="fade-left" className="text-4xl font-bold md:text-5xl lg:text-6xl capitalize text-kuning max-w-md md:max-w-[800px]">raih masa Depan dengan Belajar teknologi</h1>
           <p data-aos="fade-right" className="text-sm text-cream md:text-xl max-w-[325px] md:max-w-2xl mt-8">Transformasi Digital Membangun Masa Depan Yang Penuh Inovasi dan Peluang Tanpa batas</p>
@@ -26,8 +26,8 @@ export default function Home() {
     {/* Section 2 */}
     <div className='lg:px-40 lg:mt-72 md:mt-20 lg:py-8 px-10 -mt-20 space-y-12 font-medium'>
       <div className='lg:flex lg:justify-center lg:space-x-28 lg:space-y-0 space-y-4'>
-        <h1 className='lg:w-1/3 font-bold lg:text-5xl text-4xl'>Why?</h1>
-        <p className='lg:w-2/3 lg:text-[18px] lg:max-w-xl'>
+        <h1 data-aos="fade-right" className='lg:w-1/3 font-bold lg:text-5xl text-4xl'>Why?</h1>
+        <p data-aos="fade-left" className='lg:w-2/3 lg:text-[18px] lg:max-w-xl'>
           <span>Teknologi terus berkembang dengan cepat. Dengan belajar teknologi, kamu akan dapat mengikuti perkembangan terbaru dan terus memperbarui keterampilan kamu.</span>
           <br />
           <br />
@@ -36,20 +36,20 @@ export default function Home() {
       </div>
 
       <div className='lg:flex lg:justify-center lg:space-x-28 lg:space-y-0 space-y-4'>
-        <h1 className='lg:w-1/3 font-bold lg:text-5xl text-4xl'>Future Job</h1>
-          <div className='lg:w-2/3 lg:text-[18px] lg:max-w-xl lg:space-y-6 space-y-4'>
+        <h1 data-aos="fade-right" className='lg:w-1/3 font-bold lg:text-5xl text-4xl'>Future Job</h1>
+          <div data-aos="fade-left" className='lg:w-2/3 lg:text-[18px] lg:max-w-xl lg:space-y-6 space-y-4'>
             <p>Ada banyak peluang kerja yang tersedia bagi pelajar IT, beberapa di antaranya adalah:</p>
             <ul className='flex flex-row flex-wrap md:gap-3 gap-2 font-semibold md:text-[22px]'>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Adminitrasi Sistem</li>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>AI</li>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>IoT</li>  
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Cyber Security</li>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Web Developer</li>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>UI/UX Design</li>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Cloud Engineer</li>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>DevOps</li>
-              <li className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>IT Support</li>
-              <li className='md:px-6 p-3 rounded-xl bg-ijo border-nav text-nav border-[0.5px]'>& Lainnya</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Adminitrasi Sistem</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>AI</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>IoT</li>  
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Cyber Security</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Web Developer</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>UI/UX Design</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>Cloud Engineer</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>DevOps</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-nav border-ijo border-[0.5px]'>IT Support</li>
+              <li data-aos="fade-up" className='md:px-6 p-3 rounded-xl bg-ijo border-nav text-nav border-[0.5px]'>& Lainnya</li>
             </ul>  
           </div>
         </div>
@@ -65,8 +65,8 @@ export default function Home() {
 
     {/* Section 3 */}
     <div className="lg:px-40 lg:py-12 px-10 text-ijo font-bold">
-        <h1 className="lg:text-5xl text-4xl">Facility</h1>
-        <div className="grid lg:grid-cols-3 bg-nav lg:mt-10 mt-6 rounded-xl lg:text-2xl text-xl lg:py-8 lg:px-0 px-8">
+        <h1 data-aos="fade-right" className="lg:text-5xl text-4xl">Facility</h1>
+        <div data-aos="fade-up" className="grid lg:grid-cols-3 bg-nav lg:mt-10 mt-6 rounded-xl lg:text-2xl text-xl lg:py-8 lg:px-0 px-8">
           <div className="flex lg:px-8 lg:py-0 py-6 space-x-5 items-center border-ijo lg:border-r-[0.5px] lg:border-b-0 border-b-[0.5px]">
             <img className='w-12' src='icon/wifi.svg' />
             <div>
@@ -93,13 +93,13 @@ export default function Home() {
 
     {/* Section 4 */}
     <div id='major' className="lg:px-40 py-12 px-10 font-bold lg:space-y-6 lg:-mt-8">
-        <h1 className='lg:text-5xl text-4xl lg:my-12 text-center'>Program Keahlian yang Tersedia</h1>
+        <h1 data-aos="fade-right" className='lg:text-5xl text-4xl lg:my-12 text-center'>Program Keahlian yang Tersedia</h1>
         <div className='grid lg:grid-cols-2 lg:space-x-12 lg:text-[28px] lg:space-y-0 space-y-4 text-xl mt-12'>
           <div>
-            <div className='flex justify-center items-center bg-nav rounded-3xl py-20 px-6 lg:px-0'>
+            <div data-aos="fade-up" className='flex justify-center items-center bg-nav rounded-3xl py-20 px-6 lg:px-0'>
               <Image src={'icon/rpl-img.svg'} width={400} height={400} alt='rpl-image' />
             </div>
-              <div className='py-6 flex justify-around items-center lg:space-x-16 space-x-8'>
+              <div data-aos="fade-right" className='py-6 flex justify-around items-center lg:space-x-16 space-x-8'>
                 <div>
                   <p className='lg:text-lg text-[15px] font-normal'>PPLG</p>
                   <h1 className='lg:leading-7 leading-[22px]'>PENGEMBANGAN<br /> PERANGKAT LUNAK & GIM</h1>
@@ -110,10 +110,10 @@ export default function Home() {
             </div>
           </div>
           <div>
-            <div className='flex justify-center items-center bg-nav rounded-3xl lg:py-8 lg:px-0 p-12 '>
+            <div data-aos="fade-up" className='flex justify-center items-center bg-nav rounded-3xl lg:py-8 lg:px-0 p-12 '>
               <Image src={'icon/tkj-img.svg'} width={320} height={320} alt='tjkt-image' />
             </div>
-            <div className='py-6 flex justify-around items-center lg:space-x-16 space-x-8'>
+            <div data-aos="fade-right" className='py-6 flex justify-around items-center lg:space-x-16 space-x-8'>
                 <div>
                   <p className='lg:text-lg text-[15px] font-normal'>TJKT</p>
                   <h1 className='lg:leading-7 leading-[22px]'>TEKNIK JARINGAN<br /> KOMPUTER & TELEKOMUNIKASI</h1>
